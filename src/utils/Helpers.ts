04b718c6b04a841bf080ba2ae7c6eae1ccf79778
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-05-01 12:03:50 +0200

    RRP-5: Create TableRow component

---
+++ src/utils/Helpers.ts
@@ -0,0 +1,16 @@
+export interface Player {
+  id: number;
+  firstName: string;
+  lastName: string;
+  rating: number;
+  country: string;
+}
+
+export const RegistrationTableColumns = [
+  "X",
+  "First Name",
+  "Last Name",
+  "Rating",
+  "Country",
+  "Edit / Remove",
+];
