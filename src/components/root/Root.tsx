26474aeeaa42d0dedad29dfe8d07c793086cfeb2
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-05-01 17:23:05 +0200

    RRP-7: Add router outlet context and navigate to pairings on click

--- src/components/root/Root.tsx
+++ src/components/root/Root.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./Root.scss";
+import { Player, Round, Tournament } from "../../utils/Helpers";
+
+const staticPlayers: Player[] = [
+  {
+    id: 1,
+    firstName: "Magnus",
+    lastName: "Carlsen",
+    rating: 2853,
+    country: "Norway",
+  },
+  {
+    id: 2,
+    firstName: "Ian",
+    lastName: "Nepomniachtchi",
+    rating: 2795,
+    country: "Russia",
+  },
+];
 
 const Root = () => {
+  const [tournament, setTournament] = React.useState<Tournament>({
+    rounds: [] as Round[],
+    isFinished: false,
+  });
+  const [players, setPlayers] = React.useState<Player[]>(staticPlayers);
+
   return (
     <>
       <main className="main-content">
-        <Outlet context={{}} />
+        <Outlet
+          context={{
+            tournament,
+            setTournament,
+            players,
+            setPlayers,
+          }}
+        />
       </main>
     </>
   );
